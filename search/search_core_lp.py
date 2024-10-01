f39d6b14f42167d7796e15da57a3f38ea15d0a8a
Lequan Lin <98076638+lequanlin@users.noreply.github.com>
2024-10-01 16:23:09 +1000

    Update search_core_lp.py

--- search/search_core_lp.py
+++ search/search_core_lp.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 
-@author: Lequan Lin
-
 This is the code to support searching algorithms for GNNs on link prediction.
 
 Designed based on https://github.com/tomonori-masui/graph-neural-networks/blob/main/gnn_pyg_implementations.ipynb
